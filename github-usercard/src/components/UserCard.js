5c29bb328ab68d6ee37145694482f000c923416c
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-09-07 14:53:40 -0700

    mvp

--- github-usercard/src/components/UserCard.js
+++ github-usercard/src/components/UserCard.js
@@ -7,8 +7,10 @@ function UserCard(props) {
         <>
             <div className="user-card">
                 <img src={props.user.avatar_url} alt="github user avatar"/>
-                <h2>{props.user.name}</h2>
-                <h4>Username: {props.user.login}</h4>
+                <div className="name-tag">
+                    <h2>{props.user.name}</h2>
+                    <h4>Username: {props.user.login}</h4>
+                </div>
                 <div className="gen-info closed">
                     <p>Location: {props.user.location}</p>
                     <p>Repositories: {props.user.public_repos}</p>
